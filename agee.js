1741698da5644e939b72064278325f5d5b5c5f23
StowexD <86312923+StowexD@users.noreply.github.com>
2021-06-22 23:42:21 +0800

    123
    213

---
+++ agee.js
@@ -0,0 +1,7 @@
+agreement
+Party A:Server owner / channel opener
+Party B:Server user / server renter
+1.Party B shall not use the communication system of the server in violation of the laws and regulations of the people's Republic of China.
+2.Party B shall not attack the server with DDoS traffic and any behavior that endangers the server and channel.
+3.Whatever party B does in the server has nothing to do with Party A.
+4.Party B shall not disclose any IP address and domain name of the server in any form.
